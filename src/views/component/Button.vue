c208aa8e5ebebdaed4ef02c3baacb16d536d5d04
顾家伟 <328664896@qq.com>
2018-05-10 17:11:14 +0800

    enchance

--- src/views/component/Button.vue
+++ src/views/component/Button.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="main">
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <div>
+      <el-button type="primary" size="large">大型按钮</el-button>
+      <el-button type="primary">正常按钮</el-button>
+      <el-button type="primary" size="small">小型按钮</el-button>
+    </div>
+    <div>
+      <el-button type="text">文字按钮</el-button>
+    </div>
+    <div>
+      <el-button :plain="true" :disabled="true">禁用按钮</el-button>
+    </div>
   </div>
 </template>
 
