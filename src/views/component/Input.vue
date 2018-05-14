3762c790a075666082c460f3d9882234668b7e64
顾家伟 <328664896@qq.com>
2018-05-14 10:39:01 +0800

    enchance

--- src/views/component/Input.vue
+++ src/views/component/Input.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="main">
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-input
+      type="textarea"
+      :rows="2"
+      placeholder="请输入内容"
+      v-model="textarea">
+    </el-input>
+    <el-input-number v-model="num" :controls="false"></el-input-number>
   </div>
 </template>
 
@@ -7,7 +15,9 @@
 export default {
   data() {
     return {
-
+      input:'',
+      textarea:'',
+      num:1
     }  
   },
   methods: {
