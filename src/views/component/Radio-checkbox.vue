4b55143535fbf26dac124dbb1ad1eb7e1df9cfcd
顾家伟 <328664896@qq.com>
2018-05-14 14:10:33 +0800

    enchance

--- src/views/component/Radio-checkbox.vue
+++ src/views/component/Radio-checkbox.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="main">
+    <el-radio class="radio" v-model="radio" label="1">备选项</el-radio>
+    <el-radio class="radio" v-model="radio" label="2">备选项</el-radio>
+    <el-checkbox-group v-model="checkList">
+    <el-checkbox label="复选框 A"></el-checkbox>
+    <el-checkbox label="复选框 B"></el-checkbox>
+    <el-checkbox label="复选框 C"></el-checkbox>
+  </el-checkbox-group>
   </div>
 </template>
 
@@ -7,7 +14,8 @@
 export default {
   data() {
     return {
-
+      radio:null,
+      checkList:[]
     }  
   },
   methods: {
