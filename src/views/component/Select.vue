1e6b359b20a2f9bd6c7b6001f464551c003f3547
顾家伟 <328664896@qq.com>
2018-05-14 15:54:05 +0800

    enchance

--- src/views/component/Select.vue
+++ src/views/component/Select.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="main">
     选择水果(单选):
+    <el-select v-model="value" placeholder="请选择">
+      <el-option
+        v-for="item in list"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
 
     选择水果(多选):
+    <el-select v-model="value2" multiple placeholder="请选择">
+      <el-option
+        v-for="item in list"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
     
     用户数据(远程数据):
+    <el-select v-model="value3" placeholder="请选择">
+      <el-option
+        v-for="item in remoteList"
+        :key="item.value"
+        :label="item.name"
+        :value="item.value">
+      </el-option>
+    </el-select>
   </div>
 </template>
 
@@ -25,13 +49,18 @@ export default {
         label: '荔枝',
         value: 4,
       }],
+      value: '',
+      value2: '',
+      value3: '',
       remoteList: []
     }  
   },
   methods: {
     fetchRemoteList() {
       this.$http.get('https://jsonplaceholder.typicode.com/users').then(data => {
-        // 你的代码在这里
+        debugger
+        this.remoteList = data.data
+        debugger
       })
     }
   },
